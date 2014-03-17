152cbc25933374208a88155b5521eb8efc39472a
viprhz <viprhz@gmail.com>
2014-03-17 22:23:59 +0800

    update annotations

--- DailyCount.sh
+++ DailyCount.sh
@@ -3,13 +3,12 @@
 # 2014-01-24
 # Viprhz
 
-# 打印日志
+# Print Log
 logFile=~/Library/Preferences/CountDownloadLog.txt
 # exec 1>>${logFile}
 # exec 2>>${logFile}
 
-#统计之前一个小时的下载数量
-
+#Count downloads file
 cminTime=30
 DownloadPath=~/Downloads/
 infoPath=~/Documents/info.html   
@@ -18,37 +17,33 @@ fileName="MacBooster*"
 fileSize=$((12 * 1000 *1024))
 if [[ -f $infoPath ]];then
 		lastCount=`awk '/TotalDownload/ {print $4}' ${infoPath}`
-		currentCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -cmin -${cminTime}|wc -l` && echo "$(date "+%Y-%m-%d %H:%M:%S") TotalDownload ${currentCount}" >${infoPath}
+		currentCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -cmin -${cminTime}|wc -l`
+		echo "$(date "+%Y-%m-%d %H:%M:%S") TotalDownload ${currentCount}" >${infoPath}
 		totalCount=$((lastCount+currentCount))
-		else
-			totalCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -cmin -${cminTime}|wc -l` && echo "$(date "+%Y-%m-%d %H:%M:%S") TotalDownload ${totalCount}" >${infoPath}
+	else
+		totalCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -cmin -${cminTime}|wc -l` 
+		echo "$(date "+%Y-%m-%d %H:%M:%S") TotalDownload ${totalCount}" >${infoPath}
 
-	fi
+fi
 
-
-
-
-# 区分机器.
 ComputerName=`ifconfig|awk '/ether/ {print $2}'|head -1`
 sendAddress="http://iobit.herokuapp.com/push"
 
 
-#发送统计数量到网站服务器
+#Send data to WebServer
 for ((i=1;i<4;i++))
 {
 
-	
-
-	#判断下载数量，如果大于1就发送统计数据。
 	if [[ "${totalCount}" -ge 1 ]];then
 
-		curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"mac_address\":\"${ComputerName}\",\"downloads\":${totalCount}}" ${sendAddress} >>${infoPath}
+		curl -H "Accept: application/json" -H "Content-type: application/json" -X POST \
+		-d "{\"mac_address\":\"${ComputerName}\",\"downloads\":${totalCount}}" ${sendAddress} >>${infoPath}
 
 		countUpload=`awk '/upload_success/' ~/Documents/info.html|wc -l`
 
-			#判断是否发送成功，如果成功就删除下载文件。
+			#If send data success, then delete downloads file.
 			if [[ "${countUpload}" -ge 1 ]];then
- 				echo "$(date "+%Y-%m-%d %H:%M:%S") 成功发送统计数量到服务器" && rm -rf ${infoPath}
+ 				echo "$(date "+%Y-%m-%d %H:%M:%S") Success to send" && rm -rf ${infoPath}
 				find ~/Downloads/ -name ${fileName} -a -cmin +5 | xargs rm -rf
 				break;
 			else
@@ -61,12 +56,7 @@ for ((i=1;i<4;i++))
 			fi
 			
 	fi
-
-	echo "$(date "+%Y-%m-%d %H:%M:%S") 第${i}次发送统计数据"
+	echo "$(date "+%Y-%m-%d %H:%M:%S") The${i}times send"
 	sleep 2
 }
 
-
-
-
-				
