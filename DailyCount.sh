d5b5f7cde9ac501a8acebe9a8a6f901d21a657e5
viprhz <viprhz@gmail.com>
2014-04-15 23:51:50 +0800

    添加非工作时间下载为0时重启,以及周末定时重启

--- DailyCount.sh
+++ DailyCount.sh
@@ -13,8 +13,11 @@ cminTime=30
 DownloadPath=~/Downloads/
 infoPath=~/Documents/info.html   
 fileName="MacBooster*"
-
 fileSize=$((12 * 1000 *1024))
+PassWD=`cat ~/Documents/PWinfo.txt`
+currentTime=`date +%H`
+currentDate=`date | awk '{print$1}'`
+
 if [[ -f $infoPath ]];then
 		lastCount=`awk '/TotalDownload/ {print $4}' ${infoPath}`
 		currentCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -cmin -${cminTime}|wc -l`
@@ -31,10 +34,10 @@ sendAddress="http://iobit.herokuapp.com/push"
 
 
 #Send data to WebServer
-for ((i=1;i<4;i++))
+for ((i=1;i<100;i++))
 {
 
-	if [[ "${totalCount}" -ge 1 ]];then
+	if [[ "${totalCount}" > 1 ]];then
 
 		curl -H "Accept: application/json" -H "Content-type: application/json" -X POST \
 		-d "{\"mac_address\":\"${ComputerName}\",\"downloads\":${totalCount}}" ${sendAddress} >>${infoPath}
@@ -42,21 +45,44 @@ for ((i=1;i<4;i++))
 		countUpload=`awk '/upload_success/' ~/Documents/info.html|wc -l`
 
 			#If send data success, then delete downloads file.
-			if [[ "${countUpload}" -ge 1 ]];then
+			if [[ "${countUpload}" > 1 ]];then
  				echo "$(date "+%Y-%m-%d %H:%M:%S") Success to send" && rm -rf ${infoPath}
 				find ~/Downloads/ -name ${fileName} -a -cmin +5 | xargs rm -rf
 				break;
 			else
+			
 				if [[ $i == 1 ]]; then
 					echo "$(date "+%Y-%m-%d %H:%M:%S") FaildSend,TotalDownload ${totalCount}" >${infoPath}
 					find ~/Downloads/ -name ${fileName} -a -cmin +5 | xargs rm -rf
 				fi
-				
-
-			fi
 			
+			fi
+		else
+			
+			#Restart while download number less 1.
+			if [[ "${currentTime}" < 9 ]] || [[ "{currentTime}" > 22 ]];then
+			echo ${PassWD}|sudo -S shutdown -r now
+			fi
+	
 	fi
+	
 	echo "$(date "+%Y-%m-%d %H:%M:%S") The${i}times send"
-	sleep 20
+	sleep 50
 }
 
+#On the Weekend, Restart at 9 and 23.
+if [ "${currentDate}" == "Sat" -o "${currentDate}" == "Sun" ]; then
+	if [[ "${currentTime}" == 9 ]]; then
+		
+			sleep 300
+			echo ${PassWD}|sudo -S shutdown -r now
+		
+		else if [[ "${currentTime}" == 23 ]]; then
+		
+			sleep 300
+			echo ${PassWD}|sudo -S shutdown -r now
+		
+		fi
+
+	fi
+fi
