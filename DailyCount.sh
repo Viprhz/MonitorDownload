05b7cbe2545edb55b8d8bdf0bc36936ce1f7c53b
viprhz <viprhz@gmail.com>
2014-05-04 17:56:26 +0800

    优化结构

--- DailyCount.sh
+++ DailyCount.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
+set -o nounset
+set -o errexit
 # 脚本作用:统计下载文件的数量,并把统计数据发送到网站服务器.
 # 2014-01-24
 # Viprhz
 
-# Print Log
-logFile=~/Library/Preferences/CountDownloadLog.txt
-# exec 1>>${logFile}
-# exec 2>>${logFile}
-
-#Count downloads file
-cminTime=60
+fileName="MacBooster*"
+currentTime=`date +%H`
+startTime=$(date "+%M")
+fileSize=$((12 * 1000 *1024))
+currentDate=`date | awk '{print$1}'`
+PassWD=`cat ~/Documents/PWinfo.txt`
+ComputerName=`ifconfig|awk '/ether/ {print $2}'|head -1`
+sendAddress="http://iobit.herokuapp.com/push"
+cminTime=360
 DownloadPath=~/Downloads/
 infoPath=~/Documents/info.html   
-fileName="MacBooster*"
-fileSize=$((12 * 1000 *1024))
-PassWD=`cat ~/Documents/PWinfo.txt`
-currentTime=`date +%H`
-currentDate=`date | awk '{print$1}'`
+logFile=~/Library/Preferences/CountDownloadLog.txt
 
+echo "#################### $(date "+%Y-%m-%d %H:%M:%S") Start Script. ####################" >>${logFile}
+
+#Count downloads file
 if [[ -f $infoPath ]];then
-		lastCount=`awk '/TotalDownload/ {print $4}' ${infoPath}`
+		lastCount=`awk '/TotalDownload/ {print $5}' ${infoPath}`
 		currentCount=`find ${DownloadPath} -size +${fileSize}c -a -name ${fileName} -a -cmin -${cminTime}|wc -l`
 		totalCount=$((lastCount+currentCount))
 		#echo "$(date "+%Y-%m-%d %H:%M:%S") TotalDownload ${totalCount}" >${infoPath}
@@ -29,13 +32,11 @@ if [[ -f $infoPath ]];then
 
 fi
 
-ComputerName=`ifconfig|awk '/ether/ {print $2}'|head -1`
-sendAddress="http://iobit.herokuapp.com/push"
-
 
 #Restart while download number less 1.
 if [[ "${totalCount}" -eq 0 ]];then
 	if [[ "${currentTime}" -lt 9 ]] || [[ "${currentTime}" -gt 22 ]];then
+	echo "$(date "+%Y-%m-%d %H:%M:%S") download number less 1, Restart." >>${logFile}
 	echo ${PassWD}|sudo -S shutdown -r now
 	fi
 fi
@@ -53,38 +54,24 @@ for ((i=1;i<100;i++))
 		countUpload=`awk '/upload_success/' ~/Documents/info.html|wc -l`
 
 			#If send data success, then delete downloads file.
-			if [[ "${countUpload}" -ge 1 ]];then
- 				echo "$(date "+%Y-%m-%d %H:%M:%S") Success to send, TotalDownload ${totalCount}" >>${logFile}
+			if [[ "${countUpload}" -eq 1 ]];then
+ 				echo "$(date "+%Y-%m-%d %H:%M:%S") The ${i} times Success to send, TotalDownload ${totalCount}" >>${logFile}
 				find ~/Downloads/ -name ${fileName} -a -cmin +5 | xargs rm -rf
 				break;
 			else
-			
-				if [[ $i == 1 ]]; then
-					echo "$(date "+%Y-%m-%d %H:%M:%S") FaildSend,TotalDownload ${totalCount}" >>${logFile}
-					find ~/Downloads/ -name ${fileName} -a -cmin +5 | xargs rm -rf
-				fi
+					echo "$(date "+%Y-%m-%d %H:%M:%S") The ${i} times FaildSend,TotalDownload ${totalCount}" >>${logFile}
+					echo "$(date "+%Y-%m-%d %H:%M:%S") Last TotalDownload ${totalCount}" >${infoPath}
 			
 			fi
 	
 	fi
-	
-	echo "$(date "+%Y-%m-%d %H:%M:%S") The ${i} times send"
-	sleep 50
+
+now=$(date "+%M")
+if (( $startTime - $now == 50 )); then
+break;
+fi
+sleep 50
+
 }
 
-#On the Weekend, Restart at 9 and 23.
-if [ "${currentDate}" == "Sat" -o "${currentDate}" == "Sun" ]; then
-	if [[ "${currentTime}" == 9 ]]; then
-		
-			sleep 300
-			echo ${PassWD}|sudo -S shutdown -r now
-		
-		else if [[ "${currentTime}" == 23 ]]; then
-		
-			sleep 300
-			echo ${PassWD}|sudo -S shutdown -r now
-		
-		fi
-
-	fi
-fi
+echo "#################### $(date "+%Y-%m-%d %H:%M:%S") End Script. ####################" >>${logFile}
